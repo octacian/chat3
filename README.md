3ba697acbc2bb6677a863ff2dfb66ed6235b6b69
octacian <enduffy2014@outlook.com>
2017-06-26 19:18:19 -0700

    Handle old clients by protocol version Prevent chat duplication and colourization issues on old clients.

--- README.md
+++ README.md
@@ -2,7 +2,10 @@
 
 Enhanced Chat [chat3]
 =======================
+**Notice:** The client experience with chat3 can be improved by using Minetest 0.4.16 on the server. In order to use chat3 at its full potential, you need a version of Minetest that includes [this commit](https://github.com/minetest/minetest/commit/1ef9eee31133a3001ed0c642df5cbe54169850de) (April 27th, 2017).
 
 Yes, chat3 seems a whole lot like a spinoff of chat2, however, it (mostly) isn't. It isn't that much unlike chat3, except for one thing: rather than cluttering the screen with a second chat window, chat3 just uses the default Minetest chat. Then it's entirely up to the player to configure the chat placement, size, and anything else on their client.
 
 Messages are highlighted in a light blue to players who are within 12 blocks of the sender (configurable with `chat3.highlight_near` in `minetest.conf`, if set to `0` positional highlighting is ignored). Messages in which the receiving player's name is mentioned are highlighted in a light green, as are PMs. Messages in which the first character is a `!` are highlighted in red to all players (also known as a shout).
+
+If the server is using Minetest 0.4.16, which has the ability to properly retreive the protocol version of the client without requiring a debug build, chat3 will automatically adjust its message handling to work with older clients as well. This means that clients before 0.4.16 will not see chat duplication on their side, and colour codes will not be sent to clients that do not support them.
--- init.lua
+++ init.lua
@@ -2,6 +2,23 @@
 
 local near = minetest.setting_get("chat3.highlight_near") or 12
 
+-- [function] Colorize
+local function colorize(prot, colour, msg)
+	if prot and prot >= 27 then
+		return minetest.colorize(colour, msg)
+	else
+		return msg
+	end
+end
+
+local prot = {}
+-- [event] On join player
+minetest.register_on_joinplayer(function(player)
+	local name = player:get_player_name()
+	local info = minetest.get_player_information(name)
+	prot[name] = info.protocol_version
+end)
+
 -- [event] On chat message
 minetest.register_on_chat_message(function(name, msg)
   local sender = minetest.get_player_by_name(name)
@@ -10,35 +27,38 @@ minetest.register_on_chat_message(function(name, msg)
     local rname  = player:get_player_name()
     local colour = "#ffffff"
 
-    -- Check for near
-    if near ~= 0 then -- and name ~= rname then
-      if vector.distance(sender:getpos(), player:getpos()) <= near then
-        colour = "#88ffff"
-      end
-    end
+		local vers = prot[rname]
+		if not vers or (vers and (vers >= 29 or (vers < 29 and name ~= rname))) then
+			-- Check for near
+	    if near ~= 0 then -- and name ~= rname then
+	      if vector.distance(sender:getpos(), player:getpos()) <= near then
+	        colour = "#88ffff"
+	      end
+	    end
 
-    -- Check for mentions
-    if msg:lower():find(rname:lower(), 1, true) then
-      colour = "#00ff00"
-    end
+	    -- Check for mentions
+	    if msg:lower():find(rname:lower(), 1, true) then
+	      colour = "#00ff00"
+	    end
 
-    -- Check for shout
-    if msg:sub(1, 1) == "!" then
-      colour = "#ff0000"
-    end
+	    -- Check for shout
+	    if msg:sub(1, 1) == "!" then
+	      colour = "#ff0000"
+	    end
 
-    -- if same player, set to white
-    if name == rname then
-      colour = "#ffffff"
-    end
+	    -- if same player, set to white
+	    if name == rname then
+	      colour = "#ffffff"
+	    end
 
-    -- Send message
-    minetest.chat_send_player(rname, minetest.colorize(colour, "<"..name.."> "..msg))
+	    -- Send message
+	    minetest.chat_send_player(rname, colorize(vers, colour, "<"..name.."> "..msg))
+		end
   end
 
   -- Log message
   minetest.log("action", "CHAT: ".."<"..name.."> "..msg)
-  
+
   -- Prevent from sending normally
   return true
 end)
